b7bacbad771f22ef46bf7eb3ad737863406b90bb
ralph-12 <tmdrl1026@naver.com>
2022-09-07 00:07:40 +0900

    add annotation

--- app/src/main/java/com/example/kotlin_flow/FlowBuilders.kt
+++ app/src/main/java/com/example/kotlin_flow/FlowBuilders.kt
@@ -3,7 +3,7 @@ package com.example.kotlin_flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.runBlocking
 
-
+// Convert an integer range to a flow
 fun main() = runBlocking<Unit> {
     (1..3).asFlow().collect { value -> println(value) }
 }
