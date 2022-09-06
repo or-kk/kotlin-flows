e07043655f38d2fa85dc68d85cb9998969463e34
ralph-12 <tmdrl1026@naver.com>
2022-09-07 00:02:23 +0900

    add flow cancellation example

---
+++ app/src/main/java/com/example/kotlin_flow/FlowCancellationBasics.kt
@@ -0,0 +1,22 @@
+package com.example.kotlin_flow
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeoutOrNull
+
+fun flowCancellationBasics(): Flow<Int> = flow {
+    for (i in 1..3) {
+        delay(100)
+        println("Emitting $i")
+        emit(i)
+    }
+}
+
+fun main() = runBlocking<Unit> {
+    withTimeoutOrNull(250) {
+        flowCancellationBasics().collect { value -> println(value) }
+    }
+    println("Done")
+}
