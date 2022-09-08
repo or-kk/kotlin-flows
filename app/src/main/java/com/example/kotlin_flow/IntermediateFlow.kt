b63d1b23b382ace10819712a6c9d95b82d91d3db
ralph-12 <tmdrl1026@naver.com>
2022-09-08 20:25:16 +0900

    add intermediate flow

---
+++ app/src/main/java/com/example/kotlin_flow/IntermediateFlow.kt
@@ -0,0 +1,17 @@
+package com.example.kotlin_flow
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
+
+suspend fun performRequest(request: Int): String {
+    delay(1000)
+    return "response $request"
+}
+
+fun main() = runBlocking<Unit> {
+    (1..3).asFlow()
+        .map { request -> performRequest(request) }
+        .collect { response -> println(response)}
+}
