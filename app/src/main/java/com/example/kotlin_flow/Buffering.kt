9ad4642fc1e61196eb75664317af6bb6a3772a34
ralph-12 <tmdrl1026@naver.com>
2022-09-09 19:39:35 +0900

    add flow example

---
+++ app/src/main/java/com/example/kotlin_flow/Buffering.kt
@@ -0,0 +1,27 @@
+package com.example.kotlin_flow
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
+
+fun simpleBuffering(): Flow<Int> = flow {
+    for (i in 1..3) {
+        delay(100)
+        emit(i)
+    }
+}
+
+fun main() = runBlocking<Unit> {
+    val time = measureTimeMillis {
+        simpleBuffering()
+            .buffer() // buffer emissions, don't wait
+            .collect { value ->
+                delay(300) // pretend we are processing it for 300 ms
+                println(value)
+            }
+    }
+    println("Collected in $time ms")
+}
---
+++ app/src/main/java/com/example/kotlin_flow/Conflation.kt
@@ -0,0 +1,27 @@
+package com.example.kotlin_flow
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
+
+fun conflation(): Flow<Int> = flow {
+    for (i in 1..3) {
+        delay(100)
+        emit(i)
+    }
+}
+
+fun main() = runBlocking<Unit> {
+    val time = measureTimeMillis {
+        conflation()
+            .conflate() // conflate emissions, don't process each one
+            .collect { value ->
+                delay(300) // pretend we are processing it for 300 ms
+                println(value)
+            }
+    }
+    println("Collected in $time ms")
+}
